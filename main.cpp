762e6f654165418aa19c5aa14c054a9ee90e7255
MTsghub <mohamedtami2007@gmail.com>
2024-10-15 21:43:52 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,83 @@
+// main.cpp
+
+// Portions you are to complete are marked with a TODO: comment.
+// We've provided some incorrect return statements (so indicated) just
+// to allow this skeleton program to compile and run, albeit incorrectly.
+// The first thing you probably want to do is implement the trivial
+// functions (marked TRIVIAL).  Then get Arena::display going.  That gives
+// you more flexibility in the order you tackle the rest of the functionality.
+// As you finish implementing each TODO: item, remove its TODO: comment.
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include "Game.h"
+using namespace std;
+
+
+
+///////////////////////////////////////////////////////////////////////////
+//  main()
+///////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    // Initialize the random number generator.  (You don't need to
+    // understand how this works.)
+    srand(static_cast<unsigned int>(time(0)));
+
+    // Create a game
+    // Use this instead to create a mini-game:   Game g(3, 3, 2);
+    Game g(15, 18, 80);
+
+    // Play the game
+    g.play();
+}
+
+///////////////////////////////////////////////////////////////////////////
+//  clearScreen implementations
+///////////////////////////////////////////////////////////////////////////
+
+// DO NOT MODIFY OR REMOVE ANY CODE BETWEEN HERE AND THE END OF THE FILE!!!
+// THE CODE IS SUITABLE FOR VISUAL C++, XCODE, AND g++ UNDER LINUX.
+
+// Note to Xcode users:  clearScreen() will just write a newline instead
+// of clearing the window if you launch your program from within Xcode.
+// That's acceptable.
+
+#ifdef _MSC_VER  //  Microsoft Visual C++
+
+#include <windows.h>
+
+void clearScreen()
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(hConsole, &csbi);
+    DWORD dwConSize = csbi.dwSize.X * csbi.dwSize.Y;
+    COORD upperLeft = { 0, 0 };
+    DWORD dwCharsWritten;
+    FillConsoleOutputCharacter(hConsole, TCHAR(' '), dwConSize, upperLeft,
+        &dwCharsWritten);
+    SetConsoleCursorPosition(hConsole, upperLeft);
+}
+
+#else  // not Microsoft Visual C++, so assume UNIX interface
+
+#include <cstring>
+#include <iostream>
+
+void clearScreen()  // will just write a newline in an Xcode output window
+{
+    static const char* term = getenv("TERM");
+    if (term == nullptr || strcmp(term, "dumb") == 0)
+        std::cout << std::endl;
+    else
+    {
+        static const char* ESC_SEQ = "\x1B[";  // ANSI Terminal esc seq:  ESC [
+        std::cout << ESC_SEQ << "2J" << ESC_SEQ << "H" << std::flush;
+    }
+}
+
+#endif
